12e61016df24c827c47ac90e8378539e1fcc3d1d
Adrien Wald <adrien@genei.io>
2023-05-08 03:05:38 +0100

    feat: IconButton variants!

--- components/IconButton/IconButton.css.ts
+++ components/IconButton/IconButton.css.ts
@@ -1,8 +1,10 @@
+import { assignVars, createThemeContract } from '@vanilla-extract/css'
 import { calc } from '@vanilla-extract/css-utils'
 import { RecipeVariants, recipe } from '@vanilla-extract/recipes'
 import { focusRing } from '@/styles/common.css'
-import { mapColors } from '@/styles/palette'
+import { mapColors, shouldUseDarkForegroundText } from '@/styles/palette'
 import { vars } from '@/styles/theme.css'
+import { rem } from '@/styles/utils'
 import { pick, transformValues } from '@/utils/common'
 import { Expand } from '@/utils/types'
 
@@ -10,6 +12,23 @@ const sizes = transformValues(
   pick(vars.fontSize, ['xs', 'sm', 'md', 'lg', 'xl']),
   (value) => calc(value).multiply(2).toString(),
 )
+
+const createEmptyVariant = () => ({
+  color: '',
+  bg: '',
+  bgHover: '',
+  bgActive: '',
+  border: '',
+  borderHover: '',
+})
+
+const buttonColors = createThemeContract({
+  subtle: createEmptyVariant(),
+  light: createEmptyVariant(),
+  'light+': createEmptyVariant(),
+  filled: createEmptyVariant(),
+  outline: createEmptyVariant(),
+})
 
 export const style = recipe({
   base: {
@@ -20,6 +39,8 @@ export const style = recipe({
     border: 'none',
     userSelect: 'none',
     cursor: 'pointer',
+    textDecoration: 'none',
+    appearance: 'none',
     selectors: {
       '&:disabled, &[data-disabled]': {
         opacity: 0.5,
@@ -33,39 +54,90 @@ export const style = recipe({
     color: mapColors((getScale, color) => [
       focusRing[color],
       {
-        color: vars.palette.color[getScale(11)],
-        backgroundColor: vars.palette.color[getScale(3)],
-        ':hover': {
-          backgroundColor: vars.palette.color[getScale(4)],
-        },
-        ':active': {
-          backgroundColor: vars.palette.color[getScale(5)],
-        },
+        vars: assignVars(buttonColors, {
+          subtle: {
+            color: vars.palette.color[getScale(11)],
+            bg: 'transparent',
+            bgHover: vars.palette.color[getScale(4)],
+            bgActive: vars.palette.color[getScale(5)],
+            border: 'transparent',
+            borderHover: 'transparent',
+          },
+          light: {
+            color: vars.palette.color[getScale(11)],
+            bg: vars.palette.color[getScale(3)],
+            bgHover: vars.palette.color[getScale(4)],
+            bgActive: vars.palette.color[getScale(5)],
+            border: 'transparent',
+            borderHover: 'transparent',
+          },
+          'light+': {
+            color: vars.palette.color[getScale(11)],
+            bg: vars.palette.color[getScale(4)],
+            bgHover: vars.palette.color[getScale(5)],
+            bgActive: vars.palette.color[getScale(6)],
+            border: 'transparent',
+            borderHover: 'transparent',
+          },
+          filled: {
+            color: shouldUseDarkForegroundText(color)
+              ? vars.palette.shades['black.12']
+              : vars.palette.shades['white.12'],
+            bg: vars.palette.color[getScale(9)],
+            bgHover: vars.palette.color[getScale(10)],
+            bgActive: vars.palette.color[getScale(10)],
+            border: 'transparent',
+            borderHover: 'transparent',
+          },
+          outline: {
+            color: vars.palette.color[getScale(11)],
+            bg: vars.palette.color[getScale(2)],
+            bgHover: vars.palette.color[getScale(2)],
+            bgActive: vars.palette.color[getScale(2)],
+            border: vars.palette.color[getScale(7)],
+            borderHover: vars.palette.color[getScale(8)],
+          },
+        }),
       },
     ]),
     size: transformValues(sizes, (value) => ({
       width: value,
       height: value,
     })),
-    radius: transformValues(vars.radius, (value) => ({
-      borderRadius: value,
-    })),
+    radius: {
+      ...transformValues(vars.radius, (value) => ({
+        borderRadius: value,
+      })),
+      pill: {
+        borderRadius: rem(99999),
+      },
+    },
     inline: {
       true: {
         display: 'inline-flex',
       },
     },
-    variant: {
-      transparent: {
-        backgroundColor: 'transparent',
+    variant: transformValues(buttonColors, (variant) => ({
+      color: variant.color,
+      backgroundColor: variant.bg,
+      border: `${rem(1)} solid ${variant.border}`,
+      selectors: {
+        ['&:not(:disabled):hover, &:not([data-disabled]):hover']: {
+          backgroundColor: variant.bgHover,
+          borderColor: variant.borderHover,
+        },
+        ['&:not(:disabled):active, &:not([data-disabled]):active']: {
+          backgroundColor: variant.bgActive,
+        },
       },
-    },
+    })),
   },
 
   defaultVariants: {
     color: 'neutral',
     size: 'md',
     radius: 'sm',
+    variant: 'light',
   },
 })
 
--- pages/components.tsx
+++ pages/components.tsx
@@ -1,9 +1,12 @@
 import { NextPage } from 'next'
 import Head from 'next/head'
+import { IconPlus } from '@tabler/icons-react'
 import { Box } from '@/components/Box'
 import { Button, ButtonProps } from '@/components/Button'
+import { IconButton, IconButtonProps } from '@/components/IconButton/IconButton'
 import { Header, Main, Section } from '@/components/Semantic'
 import { Text } from '@/components/Text'
+import { rem } from '@/styles/utils'
 
 const Components: NextPage = () => {
   return (
@@ -22,28 +25,38 @@ const Components: NextPage = () => {
           </Text.H1>
         </Header>
         <Main>
-          <Section>
-            <Box spacing="xl" mx="auto" maxWidth="fit">
-              <Text.H2 color="violet" dimmed size="xl">
-                Buttons
-              </Text.H2>
-              <Buttons />
-            </Box>
-          </Section>
+          <Box mx="auto" maxWidth="fit" spacing="4xl">
+            <Section>
+              <Box spacing="xl" maxWidth="fit">
+                <Text.H2 color="violet" dimmed size="xl">
+                  Buttons
+                </Text.H2>
+                <Buttons />
+              </Box>
+            </Section>
+            <Section>
+              <Box spacing="xl" maxWidth="fit">
+                <Text.H2 color="violet" dimmed size="xl">
+                  Icon buttons
+                </Text.H2>
+                <IconButtons />
+              </Box>
+            </Section>
+          </Box>
         </Main>
       </Box>
     </>
   )
 }
 
-const variants = [
+const buttonVariants = [
   'subtle',
   'light',
   'light+',
   'outline',
   'filled',
 ] satisfies NonNullable<ButtonProps['variant']>[]
-const colors = [
+const buttonColors = [
   'neutral',
   'red',
   'orange',
@@ -56,13 +69,48 @@ const colors = [
 function Buttons() {
   return (
     <Box maxWidth="full" overflowX="auto">
-      <Box display="flex" gap="sm" flexDirection="column">
-        {variants.map((variant) => (
-          <Box key={variant} display="flex" gap="sm">
-            {colors.map((color) => (
+      <Box display="flex" gap="md" flexDirection="column">
+        {buttonVariants.map((variant) => (
+          <Box key={variant} display="flex" gap="md">
+            {buttonColors.map((color) => (
               <Button key={color} size="sm" variant={variant} color={color}>
                 Click me
               </Button>
+            ))}
+          </Box>
+        ))}
+      </Box>
+    </Box>
+  )
+}
+
+const iconButtonVariants = [
+  'subtle',
+  'light',
+  'light+',
+  'outline',
+  'filled',
+] satisfies NonNullable<IconButtonProps['variant']>[]
+const iconButtonColors = [
+  'neutral',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'violet',
+] satisfies NonNullable<IconButtonProps['color']>[]
+
+function IconButtons() {
+  return (
+    <Box maxWidth="full" overflowX="auto">
+      <Box display="flex" gap="md" flexDirection="column">
+        {iconButtonVariants.map((variant) => (
+          <Box key={variant} display="flex" gap="md">
+            {iconButtonColors.map((color) => (
+              <IconButton key={color} size="md" variant={variant} color={color}>
+                <IconPlus size={rem(16)} />
+              </IconButton>
             ))}
           </Box>
         ))}
